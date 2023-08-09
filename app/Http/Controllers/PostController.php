f0c24bfde5c0da3b5f314a1eb3717d6073f95784
Robert <roberttv17@gmail.com>
2023-08-08 18:14:53 -0600

    visualizar posts y post unico

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -14,8 +15,11 @@ class PostController extends Controller
 
     public function index(User $user)
     {
+        $posts = Post::where('user_id', $user->id)->paginate(16);
+
         return view('layouts.dashboard', [
             'user' => $user,
+            'posts' => $posts
         ]);
     }
 
@@ -30,6 +34,39 @@ class PostController extends Controller
             'titulo' => 'required|max:255',
             'descripcion' => 'required',
             'imagen' => 'required'
+        ]);
+
+        /* Post::create([
+            'titulo' => $request->titulo,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'user_id' => auth()->user()->id
+        ]); */
+
+        // Otra manera de guardar registros
+        /* $post = new Post;
+        $post->titulo = $request->titulo;
+        $post->descripcion = $request->descripcion;
+        $post->imagen = $request->imagen;
+        $post->user_id = auth()->user()->id;
+        $post->save(); */
+
+        // Otra manera de insertar ya teniendo una relacion en los modelos
+        $request->user()->posts()->create([
+            'titulo' => $request->titulo,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'user_id' => auth()->user()->id
+        ]);
+
+        return redirect()->route('posts.index', auth()->user()->username);
+    }
+
+    public function show(User $user, Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post,
+            'user' => $user
         ]);
     }
 }
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -27,4 +27,26 @@
       </div>
     </div>
   </div>
+
+  <section class="container mx-auto mt-10">
+    <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
+
+    @if ($posts->count())
+      <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        @foreach ($posts as $post)
+        <div>
+          <a href="{{ route('posts.show', ['post' => $post, 'user' => $user]) }}">
+            <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen de la publicación {{ $post->titulo }}">
+          </a>
+        </div>
+        @endforeach
+      </div>
+
+      <div class="my-10">
+        {{ $posts->links('pagination::tailwind') }}
+      </div>
+    @else
+      <p class="text-gray-600 uppercase text-base text-center font-bold">No hay Publicaciones</p>
+    @endif
+  </section>
 @endsection
